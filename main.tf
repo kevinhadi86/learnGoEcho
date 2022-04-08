c5167b719ce4d7d5260a6ed9588d6fda2889ad12
kevin <kevinhadinata86@gmail.com>
2022-04-08 21:32:05 +0700

    add docker buildx setup

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+# Semacem import untuk nunjukin kita ngambil resource dari mana
+# dalam case ini kita import gogole, karna kita butuh resource google
 provider "google" {
   project = "fabled-citadel-342513"
   region = "asia-southeast2"
@@ -5,7 +7,9 @@ provider "google" {
 }
 
 # Deploy image to Cloud Run
+# ngebuat resource di gcp dengan template/tipe resource google_cloud_run_service
 resource "google_cloud_run_service" "my_web_app" {
+  # field nama resourcenya
   name = "my-web-app"
   location = "asia-southeast2"
   template {
@@ -25,6 +29,8 @@ resource "google_cloud_run_service" "my_web_app" {
 }
 
 # Create public access
+# lu mau bikin sebuah iam policy data biar bisa dipake datanya ditempat lain, kek factory pattern
+# tapi ini intinya u mau dapetin sebuah data dati template ini, terus u kirimin bbrp parameter yang dibutuhin, terus hasilnya u pake ditempat lain
 data "google_iam_policy" "no_auth" {
   binding {
     members = [
@@ -35,6 +41,7 @@ data "google_iam_policy" "no_auth" {
 }
 
 # Enable public access on Cloud Run service
+# buat kasih cloud run gw, siapa aja yang boleh cloud run gw, dengan cara ngetaro policy_data yang dah didapet
 resource "google_cloud_run_service_iam_policy" "no_auth" {
   location = google_cloud_run_service.my_web_app.location
   project = google_cloud_run_service.my_web_app.project
